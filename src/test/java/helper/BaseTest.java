b7ae9013cf5bd73c818c6f9849ac37dbc624646c
Monalisha Rabha <monalisha_r@pursuitsoftware.com>
2025-02-20 10:19:48 +0530

    chrome

--- src/test/java/helper/BaseTest.java
+++ src/test/java/helper/BaseTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
@@ -28,8 +29,8 @@ public class BaseTest {
 	public void setup() {
 		
 		loadProperties(); // Load properties before setting up the driver
-//		this.driver = new ChromeDriver();
-		driver = new EdgeDriver();
+		this.driver = new ChromeDriver();
+//		driver = new EdgeDriver();
 		this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		this.driver.get(properties.getProperty("URL"));
 		this.driver.manage().window().maximize();
